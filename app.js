ed74df3a9f449ca168339a6a763574dc83f1f399
Hyan07 <130313209+Hyan07@users.noreply.github.com>
2026-08-12 23:25:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function exibirTextoNaTela(tag, texto){
 }
 
 function exibirMensagemInicial(){
-    exibirTextoNaTela('h1', "Jogo do numero Secreto do TiNerso");
+    exibirTextoNaTela('h1', "Jogo do numero Secreto do HyHy");
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
